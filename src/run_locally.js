eff85d3572f9e76c9813ce007ebb8217f6542bd6
Jan Curn <jan.curn@gmail.com>
2017-11-22 12:14:35 +0100

    Improved run_locally.js to use upstream proxy

--- src/run_locally.js
+++ src/run_locally.js
@@ -1,5 +1,5 @@
 /*!
- * This script runs the proxy locally on port specified by PORT environment variable
+ * This script runs the proxy with a second upstream proxy locally on port specified by PORT environment variable
  * or 8080 if not provided. This is used to manually test the proxy on normal browsing.
  *
  * node ./build/run_locally.js
@@ -9,11 +9,33 @@
  *
  */
 
-
+import http from 'http';
+import proxy from 'proxy'; // eslint-disable-line import/no-extraneous-dependencies
 import { Server } from './server';
 
+// Set up upstream proxy with no auth
+const upstreamProxyHttpServer = http.createServer();
+
+upstreamProxyHttpServer.on('error', (err) => {
+    console.error(err.stack || err);
+});
+
+const upstreamProxyServer = proxy(upstreamProxyHttpServer);
+const upstreamProxyPort = process.env.UPSTREAM_PROXY_PORT || 8081;
+upstreamProxyServer.listen(process.env.UPSTREAM_PROXY_PORT || 8081, (err) => {
+    if (err) {
+        console.error(err.stack || err);
+        process.exit(1);
+    }
+});
+
+// Setup proxy to forward to upstream
 const server = new Server({
     port: process.env.PORT || 8080,
+    // verbose: true,
+    prepareRequestFunction: () => {
+        return { requestAuthentication: false, upstreamProxyUrl: `http://127.0.0.1:${upstreamProxyPort}` };
+    },
 });
 
 server.on('requestFailed', (err) => {
