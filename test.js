522555307ffb5aac19f997d0649d69126b2ad228
Jan Curn <jan.curn@gmail.com>
2019-09-07 22:57:27 +0200

    Removed test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { createTunnel } = require('./build/tcp_tunnel.js');
-
-createTunnel('http://groups-BUYPROXIES63748:x2w8w5tyCYihKuBTpihBJDqHX@proxy.apify.com:8000', 'aws-eu-central-1-portal.0.dblayer.com:17031', {
-    verbose: true,
-    port: 9113,
-}).then((tunnel) => {
-    console.log(tunnel);
-});
