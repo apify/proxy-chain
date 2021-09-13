fb1a1b4de889a88092e2a7ccf396f1fc92a68f47
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-09-13 12:46:28 +0200

    wip: simplify chain

---
+++ src/chain.js
@@ -0,0 +1,82 @@
+const http = require('http');
+
+/**
+ * @param {http.ClientRequest} request
+ * @param {net.Socket} source
+ * @param {buffer.Buffer} head
+ * @param {*} handlerOpts
+ * @param {*} server
+ */
+const chain = (request, source, head, handlerOpts, server) => {
+    if (head.length > 0) {
+        throw new Error(`Unexpected data on CONNECT: ${head.length} bytes`);
+    }
+
+    const { upstreamProxyUrlParsed: proxy } = handlerOpts;
+
+    const options = {
+        method: 'CONNECT',
+        hostname: proxy.hostname,
+        port: proxy.port,
+        path: request.url,
+        headers: [
+            'host',
+            request.url,
+        ],
+    };
+
+    if (proxy.username || proxy.password) {
+        const auth = `${proxy.username}:${proxy.password}`;
+
+        options.headers.push('proxy-authorization', `Basic ${Buffer.from(auth).toString('base64')}`);
+    }
+
+    const client = http.request(options);
+
+    client.on('connect', (response, socket, clientHead) => {
+        socket.on('error', () => {
+            source.destroy();
+        });
+
+        if (response.statusCode !== 200) {
+            source.end('HTTP/1.1 502 Bad Gateway\r\n\r\n');
+            return;
+        }
+
+        if (clientHead.length > 0) {
+            socket.destroy(new Error(`Unexpected data on CONNECT: ${clientHead.length} bytes`));
+            return;
+        }
+
+        server.emit('tunnelConnectResponded', {
+            response,
+            socket,
+            head: clientHead,
+        });
+
+        source.write(`HTTP/1.1 200 Connection Established\r\n\r\n`);
+
+        source.pipe(socket);
+        socket.pipe(source);
+
+        socket.on('error', () => {
+            source.destroy();
+        });
+
+        source.on('error', () => {
+            socket.destroy();
+        });
+    });
+
+    client.on('error', () => {
+        source.end('HTTP/1.1 502 Bad Gateway\r\n\r\n');
+    });
+
+    source.on('error', () => {
+        client.destroy();
+    });
+
+    client.end();
+};
+
+module.exports.chain = chain;
