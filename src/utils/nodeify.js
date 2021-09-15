e949f1fafb75dd9b072e5e9ec47ff22bc0d59dcb
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-09-15 16:48:48 +0200

    revert catch removal

--- src/utils/nodeify.js
+++ src/utils/nodeify.js
@@ -2,7 +2,15 @@
 const nodeify = (promise, callback) => {
     if (typeof callback !== 'function') return promise;
 
-    promise.then((result) => callback(null, result), callback);
+    promise.then(
+        (result) => callback(null, result),
+        callback,
+        // Need to .catch because it doesn't crash the process on Node.js 14
+    ).catch((error) => {
+        process.nextTick(() => {
+            throw error;
+        });
+    });
 
     return promise;
 };
