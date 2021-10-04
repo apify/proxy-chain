b326729704a75725b2333305289c0ca2e9ce8571
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-10-04 10:15:02 +0200

    simplify export

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import { RequestError } from './request_error';
-import { Server } from './server';
-import { redactUrl } from './utils/redact_url';
-import { anonymizeProxy, closeAnonymizedProxy, listenConnectAnonymizedProxy } from './anonymize_proxy';
-import { createTunnel, closeTunnel } from './tcp_tunnel_tools';
-
-// Publicly exported functions and classes
-export {
-    Server,
-    RequestError,
-    redactUrl,
-    anonymizeProxy,
-    closeAnonymizedProxy,
-    listenConnectAnonymizedProxy,
-    createTunnel,
-    closeTunnel,
-};
+export { RequestError } from './request_error';
+export { Server } from './server';
+export { redactUrl } from './utils/redact_url';
+export { anonymizeProxy, closeAnonymizedProxy, listenConnectAnonymizedProxy } from './anonymize_proxy';
+export { createTunnel, closeTunnel } from './tcp_tunnel_tools';
