659d91e95cb5b7f1a12d4915f6a212b3f71ec7b8
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2022-10-20 15:15:12 +0200

    notes

--- src/custom_connect.ts
+++ src/custom_connect.ts
@@ -1,12 +1,15 @@
 import net from 'net';
 import type http from 'http';
 import { promisify } from 'util';
-// import { countTargetBytes } from './utils/count_target_bytes';
 
 const asyncWrite = promisify(net.Socket.prototype.write);
 
 export const customConnect = async (socket: net.Socket, server: http.Server): Promise<void> => {
-    // countTargetBytes(socket, socket);
+    // `countTargetBytes(socket, socket)` is incorrect here since `socket` is not a target.
+    // We would have to create a new stream and pipe traffic through that,
+    // however this would also increase CPU usage.
+    // Also, counting bytes here is not correct since we don't know how the response is generated
+    // (whether any additional sockets are used).
 
     await asyncWrite.call(socket, 'HTTP/1.1 200 Connection Established\r\n\r\n');
     server.emit('connection', socket);
