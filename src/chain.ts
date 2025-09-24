86ed7b02cfa6aef1463e9ba446daf9db8a8567aa
Samuel M-Lavoie <samuel.m.lavoie@gmail.com>
2025-09-24 10:43:48 -0400

    Emit an event when the tunnel has an error

--- src/chain.ts
+++ src/chain.ts
@@ -190,11 +190,31 @@ export const chain = (
         if (sourceSocket.readyState === 'open') {
             if (isPlain) {
                 sourceSocket.end();
+                
+                server.emit('tunnelError', {
+                    error,
+                    proxyChainId,
+                    customTag,
+                });
             } else {
                 const statusCode = errorCodeToStatusCode[error.code!] ?? badGatewayStatusCodes.GENERIC_ERROR;
                 const response = createCustomStatusHttpResponse(statusCode, error.code ?? 'Upstream Closed Early');
                 sourceSocket.end(response);
+
+                server.emit('tunnelError', {
+                    error,
+                    proxyChainId,
+                    statusCode,
+                    response,
+                    customTag,
+                });
             }
+        } else {
+            server.emit('tunnelError', {
+                error,
+                proxyChainId,
+                customTag,
+            });
         }
     });
 
